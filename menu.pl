fef912c4b48114f936778c6cebce73aef2a55de5
Inês Soares <ines.soares.22102003@gmail.com>
2023-11-03 17:16:27 +0000

    Added menu.pl to the project

---
+++ menu.pl
@@ -0,0 +1,18 @@
+% Write Game Logo.
+
+writeGameBanner:-
+    write('        _____                                         '),nl,
+    write('       |  __ \ ___   _____  _____  _____  ____  ___   '),nl,
+    write('       | |  | |  __\|  _  ||  _  ||  _  || ___||  __\ '),nl,
+    write('       | |__| | |   | |_| || |_| || |_| || __| | |    '),nl,
+    write('       |_____/|_|   |_____|| ____|| ____||____||_|    '),nl,
+    write('                           |_|    |_|                 ').
+
+% Main Menu.
+
+menuPlay(1):-
+    write('Let\'s begin! \n'),
+    initial_state(BoardSize, GameState), % Mudar de acordo com o nosso jogo.
+    pl1VSpl2(GameState, 1).
+
+
